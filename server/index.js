4ec4e13f1d97a2331a94c17e0576f715193ee3ef
Your Name <you@example.com>
2026-01-04 17:55:47 +0530

    Allow CORS preflight requests

--- server/index.js
+++ server/index.js
@@ -30,6 +30,7 @@ const corsOptions = {
 // Middleware
 app.use(helmet());
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
